89b630eb45863c98922e98b754d88de62022abe3
KRblL <kZhokhov04@mail.ru>
2025-04-11 12:31:16 +0300

    prepare repo

--- backend/internal/repo/users/errors.go
+++ backend/internal/repo/users/errors.go
@@ -2,4 +2,9 @@ package users
 
 import "errors"
 
-var ErrUserNotFound = errors.New("user not found")
+var (
+	ErrUserNotFound = errors.New("user not found")
+	ErrCannotDeposit    = errors.New("cannot deposit")
+	ErrCannotWithdraw   = errors.New("cannot withdraw")
+	ErrInsufficientFunds = errors.New("insufficient funds")
+)
