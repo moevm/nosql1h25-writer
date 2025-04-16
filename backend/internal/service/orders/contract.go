7f6b3d12380871e51cf6f394e8fb7a459874f414
KRblL <kZhokhov04@mail.ru>
2025-04-16 20:55:26 +0300

    finish service

--- backend/internal/service/orders/contract.go
+++ backend/internal/service/orders/contract.go
@@ -1,4 +1,12 @@
 package orders
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 //go:generate go tool mockgen -destination mocks/mock_$GOFILE -package=mocks . Service
-type Service interface{}
+type Service interface {
+	Create(ctx context.Context, in CreateIn) (primitive.ObjectID, error)
+}
--- backend/internal/service/orders/service.go
+++ backend/internal/service/orders/service.go
@@ -1,6 +1,13 @@
 package orders
 
-import "github.com/moevm/nosql1h25-writer/backend/internal/repo/orders"
+import (
+	"context"
+
+	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/moevm/nosql1h25-writer/backend/internal/repo/orders"
+)
 
 type service struct {
 	ordersRepo orders.Repo
@@ -8,4 +15,20 @@ type service struct {
 
 func New(ordersRepo orders.Repo) Service {
 	return &service{ordersRepo: ordersRepo}
+}
+
+func (s *service) Create(ctx context.Context, in CreateIn) (primitive.ObjectID, error) {
+	id, err := s.ordersRepo.Create(ctx, orders.CreateIn{
+		ClientID:       in.ClientID,
+		Title:          in.Title,
+		Description:    in.Description,
+		CompletionTime: in.CompletionTime,
+		Cost:           in.Cost,
+	})
+	if err != nil {
+		log.Errorf("service.orders.Create - s.ordersRepo.Create: %v", err)
+		return primitive.ObjectID{}, ErrCannotCreateOrder
+	}
+
+	return id, nil
 }
