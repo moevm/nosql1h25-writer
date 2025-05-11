3839e3b9915551cbf51416cc454fcd3207522675
spanwalla <lus550@yandex.ru>
2025-05-11 22:04:44 +0300

    feat: initial template

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,85 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import DemoFormAddress from './routes/demo.form.address'
+import DemoFormSimple from './routes/demo.form.simple'
+import DemoTable from './routes/demo.table'
+import DemoTanstackQuery from './routes/demo.tanstack-query'
+
+import Header from './components/Header'
+
+import TanstackQueryLayout from './integrations/tanstack-query/layout'
+
+import * as TanstackQuery from './integrations/tanstack-query/root-provider'
+
+import './styles.css'
+import reportWebVitals from './reportWebVitals.ts'
+
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanstackQueryLayout />
+    </>
+  ),
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: App,
+})
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  DemoFormAddress(rootRoute),
+  DemoFormSimple(rootRoute),
+  DemoTable(rootRoute),
+  DemoTanstackQuery(rootRoute),
+])
+
+const router = createRouter({
+  routeTree,
+  context: {
+    ...TanstackQuery.getContext(),
+  },
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <TanstackQuery.Provider>
+        <RouterProvider router={router} />
+      </TanstackQuery.Provider>
+    </StrictMode>,
+  )
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
