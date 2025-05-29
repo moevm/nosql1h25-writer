c34d13ff064fb18bd919fce0407a7342282e42c4
KRblL <kZhokhov04@mail.ru>
2025-05-29 14:04:30 +0300

    add dto

--- backend/internal/repo/users/dto.go
+++ backend/internal/repo/users/dto.go
@@ -1,7 +1,21 @@
 package users
 
+import (
+	"github.com/moevm/nosql1h25-writer/backend/internal/entity"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type CreateIn struct {
 	DisplayName string
 	Email       string
 	Password    string
+}
+
+type UpdateInput struct {
+	RequesterID           primitive.ObjectID
+	RequesterRole         entity.SystemRoleType
+	UserID                primitive.ObjectID
+	DisplayName           *string
+	FreelancerDescription *string
+	ClientDescription     *string
 }
--- backend/internal/service/users/dto.go
+++ backend/internal/service/users/dto.go
@@ -1,8 +1,22 @@
 package users
 
+import (
+	"github.com/moevm/nosql1h25-writer/backend/internal/entity"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type OperationType string
 
 const (
 	OperationTypeWithdraw OperationType = "withdraw"
 	OperationTypeDeposit  OperationType = "deposit"
 )
+
+type UpdateInput struct {
+	RequesterID           primitive.ObjectID
+	RequesterRole         entity.SystemRoleType
+	UserID                primitive.ObjectID
+	DisplayName           *string
+	FreelancerDescription *string
+	ClientDescription     *string
+}
